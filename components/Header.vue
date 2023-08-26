f1be85725d1e58cee6545d38849862af3f083879
adharamonzon <60470139+adharamonzon@users.noreply.github.com>
2023-08-26 16:54:03 +0200

    initial commit

---
+++ components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">
+    <h1>Welcome to GitHub searcher</h1>
+    <form>
+      <input type="text" placeholder=" Search Organization">
+      <button type="button">Search</button>
+    </form>
+  </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<styles>
+  .heaer {
+    margin: 10px;
+  }
+</styles>
